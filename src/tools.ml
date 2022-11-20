2ff855d903190050d3b5ba4ad5e6100c32e64e8e
Starck Paul <starck@localhost>
2022-11-20 18:09:19 +0100

    Tools functions patched

--- src/tools.ml
+++ src/tools.ml
@@ -1,10 +1,11 @@
 open Graph
 
 (* clone_nodes gr returns a new graph having the same nodes than gr, but no arc *)
-let clone_nodes gr = List.map (fun (a, _) -> (a, [])) gr
+let clone_nodes gr = n_fold gr (fun g i -> new_node g i) empty_graph
 
 (* gmap gr f maps all arcs of gr by function f *)
-let gmap gr f = List.map (fun (id, outa) -> (id,(List.map f outa)))
+let gmap gr f = let new_gr = clone_nodes gr in
+  e_fold gr (fun g id1 id2 label -> new_arc g id1 id2 (f label)) new_gr
 
 (* add_arc g id1 id2 n adds n to the value of the arc between id1 and id2.
  * If the arc does not exist, it is created. *)
