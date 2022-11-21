e7a7c2c56ede7010be5d713a68c45a47e436c732
Paul Starck <starck@insa-toulouse.fr>
2022-11-21 16:36:06 +0100

    fix and cleaning

--- algorithmes/fordFulkerson.ml
+++ algorithmes/fordFulkerson.ml
@@ -4,11 +4,7 @@ open Array
 open Stack
 
 let bfs g s t= 
-  let n = nbr_nodes g in
-  let color = Array.make n 0 in
-  let queue = Stack.creat in
-
+  assert false
 
 let ford_fulkerson g s t = 
-  let n = nbr_nodes g in
-  let c = Array.make_matrix n n 0 in
+  assert false
