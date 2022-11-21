d7723db809f1d2611952550b045b8f18d7a7d7e8
Sachoman fou <sachadarthenucq@gmail.com>
2022-11-21 16:48:59 +0100

    ff

--- algorithmes/fordFulkerson.ml
+++ algorithmes/fordFulkerson.ml
@@ -3,14 +3,20 @@ open Tools
 open Array
 open Stack
 
-let bfs g s t= 
+let bfs gres s t= 
   let n = nbr_nodes g in
   let color = Array.make n 0 in
   let pred = Array.make n 0 in
   let queue = Stack.create in
+  let u = ref s in
+  let v = ref t in
   (
     pred.(s) <- -1;
-    Stack.push s queue
+    Stack.push s queue;
+    while (!Stack.is_empty queue){
+      u := Stack.pop queue in
+      for (v = 0)
+    }
     assert False
   )
 
--- algorithmes/fordFulkesron.mli
+++ algorithmes/fordFulkesron.mli
@@ -1,3 +1,3 @@
-val bfs : int graph -> int -> int -> int array -> bool
+val bfs : int graph -> int -> int -> int list
 val edmund_karp : int  graph -> int -> int
 val ford_fulkerson : int graph -> int  -> int
