8584882c7e03733f6622ba940a6bcbcc71b084af
sachadarthenucq <sachadarthenucq@gmail.com>
2022-11-23 11:46:11 +0100

    conf

--- algorithmes/fordFulkesron.mli
+++ algorithmes/fordFulkesron.mli
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-val bfs: int graph -> int -> int -> int array -> bool
-val edmund_karp: int  graph -> int -> int
-val ford_fulkerson: int graph -> int  -> int
+
 val find_min_flow_of_path: int graph -> int list -> int graph
 val make_residual_graph: int graph -> int graph
-=======
 val bfs : int graph -> int -> int -> int list
 val edmund_karp : int  graph -> int -> int
 val ford_fulkerson : int graph -> int  -> int
->>>>>>> d7723db809f1d2611952550b045b8f18d7a7d7e8
