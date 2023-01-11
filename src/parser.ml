79e49f6ca05ca75c68e9928f3b7f7dddc860285d
Paul Starck <starck@insa-toulouse.fr>
2023-01-11 12:09:04 +0100

    temp

--- src/parser.ml
+++ src/parser.ml
@@ -1,4 +1,18 @@
 open Graph
 open Printf
 
-let from_file path1 path2 = assert false
+type path = string
+
+let from_file path1 path2 =
+  let infile1 = open_in path1 in
+  let n = int_of_string (List.hd (String.split_on_char ',' (input_line infile1))) in
+  let infile2 = open_in path2 in
+  let m = int_of_string (List.hd (String.split_on_char ',' (input_line infile1))) in
+  let names = [] in
+  let places_by_host = [] in
+  let correspondence = [] in
+  let rec loop =
+    try
+      let line = String.split_on_char ',' (input_line infile1) in
+
+    with End_of_file -> ()
