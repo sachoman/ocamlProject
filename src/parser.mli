99a3ecc1a59e170d099758ec40846020040b0752
Paul Starck <starck@insa-toulouse.fr>
2023-01-11 11:29:24 +0100

    parser.mli

---
+++ src/parser.mli
@@ -0,0 +1,7 @@
+open Graph
+open Printf
+
+(* From two files, returns the number of hosts, the number of hackers,
+   the name of the hosts and the hackers, the number of beds available of each host
+   and the the matching between hacker-host *)
+val from_file: string -> string -> int * int * string array * int array * (id * id) list
